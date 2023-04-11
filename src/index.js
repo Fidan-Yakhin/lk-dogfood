45da58ed209b3c061b2455f423489222a885acf5
Fidan <fidan.yakhin1@mail.ru>
2023-04-11 20:41:46 +0500

    DogFood

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import  ReactDOM from "react-dom/client";
+import './index.css'
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// root.render(
+//   React.createElement('h1', null, 'Hello')
+// )
+root.render(
+  <div title="Doggy">
+    <h1>Hay!!!</h1>
+    <h2>
+      <mark>React</mark></h2>
+  </div>
+)
